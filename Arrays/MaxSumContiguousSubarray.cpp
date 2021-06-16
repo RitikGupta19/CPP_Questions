e46cefade77b15b73db65ff780bd2d7ae5b4b12a
Ritik Gupta <ritikgupta1982@gmail.com>
2021-06-17 03:11:54 +0530

    Max sum in countiguous subarray calculated

---
+++ Arrays/MaxSumContiguousSubarray.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+  // arr: input array
+  // n: size of array
+  //Function to find the sum of contiguous subarray with maximum sum.
+  int maxSubarraySum(int arr[], int n)
+  {
+
+    // Your code here
+    int i, res = arr[0], maxSum = arr[0];
+    for (i = 1; i < n; i++)
+    {
+      // prev = maxSum;
+      maxSum = max(maxSum + arr[i], arr[i]);
+      if (maxSum > res)
+      {
+        res = maxSum;
+      }
+    }
+    return res;
+  }
+};
